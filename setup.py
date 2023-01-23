2b896d2521f3c209ddaaa0d93d13f749bd474a81
Darshan Patil <darshan3.patil@gmail.com>
2023-01-23 16:07:26 +0530

    structure setup done

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+
+setup(
+    name='sensor',
+    version="0.0.1",
+    author="Darshan Patil",
+    author_email="darshan3.patil@gmail.com",
+    packages= find_packages(),
+)
